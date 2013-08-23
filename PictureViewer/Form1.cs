49c36b47e090e2b89f1cabcfabb29c9f96967ad9
Victor Reynolds <vwr0527@gmail.com>
2013-08-23 13:13:34 -0500

    Simple picture viewer
    Improved from the C# tutorial. Centers image.

---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        //picture's x and y coordinates
+        private Point mouseDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //initialize to this setting
+
+            mouseDown = new Point(0, 0);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(pictureBox1_MouseMove);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(pictureBox1_MouseUp);
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+
+        private void closeButton_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Image = null;
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            }
+        }
+
+        private Padding startPadding;
+
+        private void pictureBox1_MouseDown(object sender, EventArgs e)
+        {
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            if (mouse.Button == MouseButtons.Left)
+            {
+                Point mouseDown = mouse.Location;
+
+                startPadding = pictureBox1.Padding;
+            }
+            Debug.WriteLine(startPadding);
+        }
+
+        private void pictureBox1_MouseUp(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseMove(object sender, EventArgs e)
+        {
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            if (mouse.Button == MouseButtons.Left)
+            {
+                Point mousePosNow = mouse.Location;
+
+                int deltaX = mousePosNow.X - mouseDown.X;
+                int deltaY = mousePosNow.Y - mouseDown.Y;
+
+                int newX = startPadding.Left - deltaX;
+                int newY = startPadding.Top - deltaY;
+
+                Point newPosition = new Point(newX, newY);
+                pictureBox1.Padding = new Padding(newX, newY, 0, 0);
+                pictureBox1.Refresh();
+            }
+        }
+    }
+}
